00ffedc43480fd8eaca0aab880b3e3a1ff789053
hnbm <59113991+hnbm@users.noreply.github.com>
2020-04-13 17:48:26 +0900

    2020/04/09까지의 코드 - BottomNavigationView

---
+++ app/src/main/java/com/example/boost/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.boost;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentArticle fragmentArticle = new FragmentArticle();
+    private FragmentEconomy fragmentEconomy = new FragmentEconomy();
+    private FragmentSimulation fragmentSimulation = new FragmentSimulation();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentArticle).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId()) {
+                case R.id.articleItem:
+                    transaction.replace(R.id.frameLayout, fragmentArticle).commitAllowingStateLoss();
+                    return true;
+                //break;
+                case R.id.economyItem:
+                    transaction.replace(R.id.frameLayout, fragmentEconomy).commitAllowingStateLoss();
+                    return true;
+                //break;
+                case R.id.simulationItem:
+                    transaction.replace(R.id.frameLayout, fragmentSimulation).commitAllowingStateLoss();
+                    return true;
+                //break;
+                default:
+                    return false;
+            }
+        }
+    }
+}
